e905b0d6011c633023c86fb188804dff6af8c919
Eric Rochester <erochest@gmail.com>
2023-04-18 07:23:40 -0400

    first iteration on push

--- loxrs/src/linked_list/mod.rs
+++ loxrs/src/linked_list/mod.rs
@@ -1,5 +1,8 @@
+use std::borrow::BorrowMut;
+use std::cell::Cell;
+use std::rc::Rc;
 
-type NodePtr<T> = Option<Box<Node<T>>>;
+type NodePtr<T> = Option<Rc<Cell<Node<T>>>>;
 
 struct Node<T> {
     value: T,
@@ -20,6 +23,29 @@ impl<T> LinkedList<T> {
             tail: None,
             length: 0,
         }
+    }
+
+    pub fn push(&mut self, value: T) {
+        let mut new_node = Rc::new(Cell::new(Node {
+                    value,
+                    next: None,
+                    prev: None,
+                }));
+
+        match self.tail {
+            Some(ref _old_tail ) => {
+                todo!();
+                // new_node.get_mut().prev = Some(Rc::clone(&old_tail));
+                // old_tail.next = Some(Rc::clone(&new_node));
+                // self.tail = Some(Rc::clone(&new_node));
+            },
+            None => {
+                self.head = Some(Rc::clone(&new_node));
+                self.tail = Some(Rc::clone(&new_node));
+            },
+        }
+
+        self.length += 1;
     }
 }
 
--- loxrs/src/linked_list/tests.rs
+++ loxrs/src/linked_list/tests.rs
@@ -11,3 +11,10 @@ fn when_constructing_a_linked_list_then_the_list_is_empty() {
     let list: LinkedList<usize> = LinkedList::new();
     assert_eq!(list.length, 0);
 }
+
+#[test]
+fn when_pushing_an_item_then_the_list_is_not_empty() {
+    let mut list: LinkedList<usize> = LinkedList::new();
+    list.push(1);
+    assert_eq!(list.length, 1);
+}
