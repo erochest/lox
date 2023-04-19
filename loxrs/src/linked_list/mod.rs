0364b967b86a5a926aea83e32efa17e10ea3b8f8
Eric Rochester <erochest@gmail.com>
2023-04-19 07:32:21 -0400

    push grows the list

--- loxrs/src/linked_list/mod.rs
+++ loxrs/src/linked_list/mod.rs
@@ -1,6 +1,7 @@
 use std::borrow::{BorrowMut, Borrow};
 use std::cell::{Cell, RefCell};
 use std::rc::Rc;
+use std::fmt::Debug;
 
 type NodePtr<T> = Option<Box<Node<T>>>;
 
@@ -9,19 +10,20 @@ pub struct LinkedList<T> {
     length: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Link<T> {
     Empty,
     More(Box<Node<T>>),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Node<T> {
     value: T,
     next: Link<T>,
 }
 
-impl<T> LinkedList<T> {
+impl<T> LinkedList<T>
+where T: Debug + PartialEq {
     pub fn new() -> Self {
         Self {
             head: Link::Empty,
@@ -35,24 +37,22 @@ impl<T> LinkedList<T> {
             next: Link::Empty,
         });
 
-        match self.head {
-            Link::Empty => self.head = Link::More(new_node),
-            Link::More(ref mut node) => {
-                node.next = Link::More(new_node);
-            }
+        match self.head.last_mut() {
+            Some(node) => node.next = Link::More(new_node),
+            None => self.head = Link::More(new_node),
         }
 
         self.length += 1;
     }
 
-       fn get(&self, i: usize) -> Option<&T> where T: std::fmt::Debug  {
+    fn get(&self, i: usize) -> Option<&T> {
         self.head.get(i)
     }
 }
 
 impl<T> Link<T> {
     fn get(&self, i: usize) -> Option<&T> where T: std::fmt::Debug {
-        println!("self: {:?} / i: {}", self, i);
+        // println!("self: {:?} / i: {}", self, i);
         if let Link::More(ref node) = self {
             if i == 0 {
                 Some(&node.value)
@@ -61,6 +61,48 @@ impl<T> Link<T> {
             }
         } else {
             None
+        }
+    }
+
+    fn tail(&self) -> Option<&Link<T>> {
+        if let Link::More(ref node) = self {
+            Some(&node.next)
+        } else {
+            None
+        }
+    }
+
+    fn last(&self) -> Option<&Node<T>> {
+        if let Link::More(ref node) = self {
+            node.last()
+        } else {
+            None
+        }
+    }
+
+    fn last_mut(&mut self) -> Option<&mut Node<T>> {
+        if let Link::More(ref mut node) = self {
+            node.last_mut()
+        } else {
+            None
+        }
+    }
+}
+
+impl<T> Node<T> {
+    fn last(&self) -> Option<&Node<T>> {
+        if let Link::More(ref node) = self.next {
+            node.last()
+        } else {
+            Some(self)
+        }
+    }
+
+    fn last_mut(&mut self) -> Option<&mut Node<T>> {
+        if let Link::More(ref mut node) = self.next {
+            node.last_mut()
+        } else {
+            Some(self)
         }
     }
 }
--- loxrs/src/linked_list/tests.rs
+++ loxrs/src/linked_list/tests.rs
@@ -25,9 +25,24 @@ fn when_pushing_multiple_items_then_the_list_is_not_empty() {
     let mut list: LinkedList<usize> = LinkedList::new();
     list.push(1);
     list.push(2);
+    assert_eq!(list.length, 2);
+    assert_eq!(list.get(0), Some(&1));
+    assert_eq!(list.get(1), Some(&2));
+}
+
+#[test]
+fn when_pushing_the_list_grows() {
+    let mut list: LinkedList<usize> = LinkedList::new();
+    list.push(1);
+    list.push(2);
+    assert_eq!(list.length, 2);
     list.push(3);
     assert_eq!(list.length, 3);
+    assert_eq!(list.get(0), Some(&1));
+    assert_eq!(list.get(1), Some(&2));
+    assert_eq!(list.get(2), Some(&3));
 }
+
 
 #[test]
 fn when_getting_an_item_then_the_item_is_returned() {
