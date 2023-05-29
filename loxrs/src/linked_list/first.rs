af3680f97c1ace896470d4b150e59613c1bacd60
Eric Rochester <erochest@gmail.com>
2023-05-29 16:24:22 -0400

    Bad Stack push

--- loxrs/src/linked_list/first.rs
+++ loxrs/src/linked_list/first.rs
@@ -1,3 +1,4 @@
+use std::mem;
 
 pub struct List {
     head: Link,
@@ -7,16 +8,24 @@ impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
     }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            item: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
 }
 
 enum Link {
     Empty,
-    More(i32, Box<Node>),
+    More(Box<Node>),
 }
 
 struct Node {
     item: i32,
-    next: List,
+    next: Link,
 }
 
 #[cfg(tests)]
