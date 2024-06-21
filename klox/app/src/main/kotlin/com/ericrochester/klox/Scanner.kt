27f5afc6eda88f794bdbbaf8e1a4d6cfdc22b757
Eric Rochester <erochest@gmail.com>
2024-06-21 18:32:54 -0400

    lexing lots of things

--- klox/app/src/main/kotlin/com/ericrochester/klox/Scanner.kt
+++ klox/app/src/main/kotlin/com/ericrochester/klox/Scanner.kt
@@ -22,4 +22,62 @@ class Scanner(private val source: String) {
     private fun isAtEnd(): Boolean {
         return current >= source.length
     }
+
+    private fun scanToken() {
+        val c = advance()
+        when (c) {
+            '(' -> addToken(LEFT_PAREN)
+            ')' -> addToken(RIGHT_PAREN)
+            '{' -> addToken(LEFT_BRACE)
+            '}' -> addToken(RIGHT_BRACE)
+            ',' -> addToken(COMMA)
+            '.' -> addToken(DOT)
+            '-' -> addToken(MINUS)
+            '+' -> addToken(PLUS)
+            ';' -> addToken(SEMICOLON)
+            '*' -> addToken(STAR)
+
+            '!' -> addToken(if (match('=')) BANG_EQUAL else BANG)
+            '=' -> addToken(if (match('=')) EQUAL_EQUAL else EQUAL)
+            '<' -> addToken(if (match('=')) LESS_EQUAL else LESS)
+            '>' -> addToken(if (match('=')) GREATER_EQUAL else GREATER)
+
+            '/' -> if (match('/')) {
+                // A comment goes until the end of the line.
+                while (peek() != '\n' && !isAtEnd()) advance()
+            } else {
+                addToken(SLASH)
+            }
+
+            ' ', '\r', '\t' -> {
+                // Ignore whitespace.
+            }
+            '\n' -> line++
+
+            _ -> Lox.error(line, "Unexpected character.")
+        }
+    }
+
+    private fun advance(): Char {
+        return source[current++]
+    }
+
+    private fun addToken(type: TokenType) {
+        addToken(type, null)
+    }
+
+    private fun addToken(type: TokenType, literal: Any?) {
+        tokens.add(Token(type, source.substring(start, current), literal, line))
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd() || source[current] != expected) return false
+        current++
+        return true
+    }
+
+    private fun peek(): Char {
+        if (isAtEnd()) return '\u0000'
+        return source[current]
+    }
 }
