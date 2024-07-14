3e377e218d23a69d3521c56feda3d7acb008dbe4
Eric Rochester <erochest@gmail.com>
2024-07-14 13:32:53 -0400

    todos

--- klox/app/src/main/kotlin/com/ericrochester/klox/app/App.kt
+++ klox/app/src/main/kotlin/com/ericrochester/klox/app/App.kt
@@ -21,6 +21,42 @@ val interpreter: Interpreter = Interpreter()
 var hadError = false
 var hadRuntimeError = false
 
+// TODO: We have methods on instances, but there is no way to define “static”
+// methods that can be called directly on the class object itself. Add support
+// for them. Use a class keyword preceding the method to indicate a static
+// method that hangs off the class object.
+// 
+// class Math {
+//   class square(n) {
+//     return n * n;
+//   }
+// }
+// 
+// print Math.square(3); // Prints "9".
+//
+// You can solve this however you like, but the “metaclasses” used by Smalltalk
+// and Ruby are a particularly elegant approach. Hint: Make LoxClass extend
+// LoxInstance and go from there.
+
+// TODO: Most modern languages support “getters” and “setters”—members on a
+// class that look like field reads and writes but that actually execute user-
+// defined code. Extend Lox to support getter methods. These are declared
+// without a parameter list. The body of the getter is executed when a property
+// with that name is accessed.
+// 
+// class Circle {
+//   init(radius) {
+//     this.radius = radius;
+//   }
+// 
+//   area {
+//     return 3.141592653 * this.radius * this.radius;
+//   }
+// }
+// 
+// var circle = Circle(4);
+// print circle.area; // Prints roughly "50.2655".
+
 fun main(args: Array<String>) {
   if (args.size > 1) {
     println("Usage: klox [script]")
