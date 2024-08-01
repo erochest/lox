bc352eb36b918cc9bcf5f6e771bbba746cc43919
Eric Rochester <erochest@gmail.com>
2024-08-01 17:36:04 -0400

    Scanner and token

--- loxrs/src/compiler/mod.rs
+++ loxrs/src/compiler/mod.rs
@@ -2,6 +2,30 @@ use crate::error::Result;
 use crate::scanner::Scanner;
 
 pub fn compile<S: AsRef<str>>(source: S) -> Result<()> {
-    let scanner = Scanner::new(source.as_ref().to_string());
+    let mut scanner = Scanner::new(source.as_ref().to_string());
+
+    let mut line: usize = 0;
+    loop {
+        let token = scanner.scan_token()?;
+
+        if token.line != line {
+            line = token.line;
+            println!("{:4} ", line);
+        } else {
+            print!("   | ");
+        }
+
+        println!(
+            "{:#?}. '{:width$}'",
+            token.ty,
+            token.start,
+            width = token.length
+        );
+
+        if token.ty == crate::scanner::TokenType::EOF {
+            break;
+        }
+    }
+
     Ok(())
 }
--- loxrs/src/scanner/mod.rs
+++ loxrs/src/scanner/mod.rs
@@ -1,8 +1,71 @@
+use crate::error::Result;
+
 pub struct Scanner {
     input: String,
     start: usize,
     current: usize,
     line: usize,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    For,
+    Fun,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Error,
+    EOF,
+}
+
+// TODO: I can add a lifetime to this to make this reference a slice instead of containing the
+// indexes.
+pub struct Token {
+    pub ty: TokenType,
+    pub start: usize,
+    pub length: usize,
+    pub line: usize,
 }
 
 impl Scanner {
@@ -22,5 +85,9 @@ impl Scanner {
             current: 0,
             line,
         }
+    }
+
+    pub fn scan_token(&mut self) -> Result<Token> {
+        todo!()
     }
 }
