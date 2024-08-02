1110a5b1f863ac529b26aabe72b9f4953d285dda
Eric Rochester <erochest@gmail.com>
2024-08-01 21:16:09 -0400

    More Token scanning infrastructure

--- loxrs/src/scanner/mod.rs
+++ loxrs/src/scanner/mod.rs
@@ -55,7 +55,7 @@ pub enum TokenType {
     Var,
     While,
 
-    Error,
+    Error(String),
     EOF,
 }
 
@@ -63,6 +63,16 @@ pub struct Token<'a> {
     pub ty: TokenType,
     pub token: &'a str,
     pub line: usize,
+}
+
+impl<'a> Token<'a> {
+    fn error(message: String, line: usize) -> Self {
+        Self::new(TokenType::Error(message), "", line)
+    }
+
+    fn new(ty: TokenType, token: &'a str, line: usize) -> Self {
+        Self { ty, token, line }
+    }
 }
 
 impl<'a> Scanner {
@@ -85,6 +95,25 @@ impl<'a> Scanner {
     }
 
     pub fn scan_token(&'a mut self) -> Result<Token<'a>> {
-        todo!()
+        self.start = self.current;
+
+        if self.is_at_end() {
+            return Ok(self.make_token(TokenType::EOF));
+        }
+
+        // TODO: Make this an Err(Error).
+        Ok(self.error_token("Unexpected character.".to_string()))
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.input.len()
+    }
+
+    fn make_token(&self, ty: TokenType) -> Token {
+        Token::new(ty, &self.input[self.start..self.current], self.line)
+    }
+
+    fn error_token(&self, message: String) -> Token {
+        Token::error(message, self.line)
     }
 }
